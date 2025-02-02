bbbde98973c9b6ee018a0f33aed00611f9a93f28
veroyols <vero.scholz@gmail.com>
2025-02-01 22:29:11 -0300

    get

---
+++ Presentation/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public ValuesController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(new { value = "value"});
+        }
+
+        [HttpGet("plumber")]
+        public async Task<string> Plumber([FromQuery] string msj)
+        {
+            // La URL de la API Plumber con el parámetro msj
+            
+            var url = $"http://localhost:8000/echo?msj={Uri.EscapeDataString(msj)}";
+
+            // Realizar la solicitud GET a la API Plumber
+            var response = await _httpClient.GetStringAsync(url);
+
+            return response;
+        }
+    }
+}
