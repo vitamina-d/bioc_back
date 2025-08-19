f033c79e212142b390b2678203d0549ac9e257bc
veroyols <vero.scholz@gmail.com>
2025-08-18 23:59:03 -0300

    Endpoints to R

---
+++ Presentation/Controllers/PlumberController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlumberController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _plumberBaseUrl;
+
+        public PlumberController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _plumberBaseUrl = "http://host.docker.internal:8000";
+        }
+        //primer endpoint: me trae el mensaje
+        [HttpGet("msg")]
+        public async Task<IActionResult> GetMessage([FromQuery] string msg = "msg")
+        {
+            var url = $"{_plumberBaseUrl}/echo/?msg={Uri.EscapeDataString(msg)}";
+            var response = await _httpClient.GetStringAsync(url); 
+            //var json = JsonSerializer.Deserialize<object>(response); //lo agrego en plumber o aca
+            return Ok(response);//{"msg": "The message is: 'msg'"}
+        }
+
+        [HttpGet("sequence_by_symbol")]
+        public async Task<IActionResult> GetSequenceBySymbol([FromQuery] string gene_symbol = "DHCR7", [FromQuery] bool complete = true)
+        {
+            // seq_by_symbol devuelve la secuencia completa o de exones, dado el symbol de un gen
+            //'http://localhost:8787/p/df91a627/seq_by_symbol/?gene_symbol=DHCR7&complete=true'
+            var url = $"{_plumberBaseUrl}/seq_by_symbol/?gene_symbol={gene_symbol.ToUpper()}&complete={complete}";
+            var response = await _httpClient.GetStringAsync(url);
+            return Ok(response);
+        }
+
+        [HttpGet("percent")]
+        public async Task<IActionResult> GetPercent([FromQuery] string sequence = "ATCG")
+        {
+            //percent muestra el porcentaje de bases, A / T y C/ G de una secuencia
+            //'http://localhost:8787/p/df91a627/percent/?seq=ACGT'
+            var url = $"{_plumberBaseUrl}/percent/?seq={sequence}";
+            var response = await _httpClient.GetStringAsync(url);
+
+            return Ok(response);
+        }
+
+        [HttpGet("detail")]
+        public async Task<IActionResult> GetDetail([FromQuery] string gene_symbol = "DHCR7")
+        {
+            
+            //detail muestra info de un gen, dado su symbol
+            //'http://localhost:8787/p/df91a627/detail/?symbol=DHCR7'
+            var url = $"{_plumberBaseUrl}/detail/?symbol={gene_symbol.ToUpper()}";
+            var response = await _httpClient.GetStringAsync(url);
+
+            return Ok(response);
+        }
+
+        [HttpGet("sequence_by_range")]
+        public async Task<IActionResult> GetSequenceByRange([FromQuery] string chrom = "chr11", [FromQuery] int start = 100000, [FromQuery] int end = 100100)
+        {
+            //seq_by_range devuelve la secuencia dado el cromosoma y el rango
+            //'http://localhost:8787/p/df91a627/seq_by_range/?chrom=chr11&start=71428193&end=71452868'
+            var url = $"{_plumberBaseUrl}/seq_by_range/?chrom={chrom}&start={start}&end={end}";
+            var response = await _httpClient.GetStringAsync(url);
+
+            return Ok(response);
+        }
+
+        [HttpGet("align")]
+        public async Task<IActionResult> GetAlign([FromQuery] string pattern = "ATCG", [FromQuery] string subject = "ATCG", [FromQuery] bool global = true)
+        {
+            //align devuelve el alineamiento global o local de dos secuecias
+            //'http://localhost:8787/p/df91a627/align/?pattern=AAACC&subject=ACGTC&global=true'
+            var url = $"{_plumberBaseUrl}/align/?pattern={pattern}&subject={subject}&global={global}";
+            var response = await _httpClient.GetStringAsync(url);
+
+            return Ok(response);
+        }
+    }
+}
--- Presentation/Controllers/PublicController.cs
+++ Presentation/Controllers/PublicController.cs
@@ -1,46 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
-using System.Xml.Linq;
 
 namespace Presentation.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class GenomeController : ControllerBase
+    public class PublicController : ControllerBase
     {
         private readonly HttpClient _httpClient;
 
-        public GenomeController(HttpClient httpClient)
+        public PublicController(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
-        //primer endpoint: me trae el mensaje
-        [HttpGet("msg")]
-        public async Task<IActionResult> GetMessage([FromQuery] string msg)
-        {
-            // localhost:8000
-            // otro contenedor -> host.docker.internal:8000
-
-            var url = $"http://host.docker.internal:8000/echo?msj={Uri.EscapeDataString(msg)}";
-
-            // Realizar la solicitud GET a la API Plumber
-            var response = await _httpClient.GetStringAsync(url);
-            var json = JsonSerializer.Deserialize<object>(response); //ya no devuelve lista
-            return Ok(json);
-        }
-
-        [HttpGet("seq")]
-        public async Task<IActionResult> GetBSGenomeSequence([FromQuery] string chrom = "chr11", [FromQuery] int start = 100000, [FromQuery] int end = 100100)
-        {
-            var url = $"http://host.docker.internal:8000/bsgenome?chrom={Uri.EscapeDataString(chrom)}&start={start}&end={end}";
-
-            var response = await _httpClient.GetStringAsync(url);
-
-            return Content(response, "application/json");
-        }
 
         [HttpGet("ensembl")]
-        public async Task<IActionResult> GetSequence([FromQuery] string chrom = "11", [FromQuery] int start = 100000, [FromQuery] int end = 100100)
+        public async Task<IActionResult> GetSequenceByRange([FromQuery] string chrom = "chr11", [FromQuery] int start = 100000, [FromQuery] int end = 100100)
         {
             string url = $"https://rest.ensembl.org/sequence/region/human/{chrom}:{start}..{end}?content-type=application/json";
             var response = await _httpClient.GetStringAsync(url);
