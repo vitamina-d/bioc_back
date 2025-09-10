138ac1c841486cbf34deae92ca751b307a747d08
veroyols <vero.scholz@gmail.com>
2025-09-10 11:35:47 -0300

    connect blast no anda

--- Infrastructure/Query/BlastClient.cs
+++ Infrastructure/Query/BlastClient.cs
@@ -9,25 +9,28 @@ namespace Application
 
         public BlastClient()
         {
-            _client = new DockerClientConfiguration(
-                new Uri("npipe://./pipe/docker_engine"))
-                    .CreateClient();
+            _client = new DockerClientConfiguration().CreateClient();
+
         }
 
         public async Task<string> Connect()
         {
-            var parameters = new CreateContainerParameters
+            var res = "";
+            try
             {
-                Image = "fedora/memcached",
-                HostConfig = new HostConfig
-                {
-                    DNS = new[] { "8.8.8.8", "8.8.4.4" }
-                }
-            };
-
-            //await _client.Containers.CreateContainerAsync(parameters);
-            var containers = await _client.Containers.ListContainersAsync(new ContainersListParameters { All = true });
-            return containers.ToString();
+                IList<ContainerListResponse> containers = await _client.Containers.ListContainersAsync(
+                    new ContainersListParameters
+                    {
+                        Limit = 10,
+                    });
+            } 
+            catch (Exception e)
+            {
+                res = $"catch: {e}";
+                
+            }
+                        
+            return res;
 
         }
 
