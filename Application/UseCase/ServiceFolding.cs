a2a690bd2dd1a5a79c04acb7b42ed0b06e684730
veroyols <vero.scholz@gmail.com>
2025-10-06 23:56:12 -0300

    get estructures

--- Application/UseCase/ServiceFolding.cs
+++ Application/UseCase/ServiceFolding.cs
@@ -3,12 +3,12 @@
     public class ServiceFolding : IServiceFolding
     {
         private readonly INeurosnapClient _neurosnapClient; //neurosnap
-        private readonly IPythonClient _pyClient; //biopython
+        private readonly IPythonClient _pythonClient; //biopython
         private readonly IPublicApiClient _publicClient; //page
-        public ServiceFolding(INeurosnapClient neurosnapClient, IPythonClient pyClient, IPublicApiClient publicClient)
+        public ServiceFolding(INeurosnapClient neurosnapClient, IPythonClient pythonClient, IPublicApiClient publicClient)
         {
             _neurosnapClient = neurosnapClient;
-            _pyClient = pyClient;
+            _pythonClient = pythonClient;
             _publicClient = publicClient;
         }
         public async Task<string> GetEstructures(string jobId, string pdbId)
@@ -20,13 +20,13 @@
             var referencePdb = await _publicClient.DownloadPdb(pdbId);
 
             //py: (prediccion, pdbid) -> align
-            var alignPdbs = await _pyClient.GetAlignProtein(predictionPdb, referencePdb);
+            var alignPdbs = await _pythonClient.GetAlignProtein(predictionPdb, referencePdb);
             return predictionPdb;
         }
         public async Task<string> InitFoldingJob(string nucleotides, int frame)
         {
             //py: (seq, frame) -> AA
-            var aminoacidSequence = await _pyClient.GetAminoAcidSeq(nucleotides, frame);
+            var aminoacidSequence = await _pythonClient.GetAminoAcidSeq(nucleotides, frame);
             var jobId = "123456789";
             //var jobId = await _neurosnapClient.InitJob(aminoacidSequence);
             return jobId;
